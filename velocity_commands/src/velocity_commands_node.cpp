f02e050e3f02710224bb8eacebe915d5954ca180
Djordje Vukcevic <djordje.vukcevic@smail.inf.h-brs.de>
2017-12-04 19:56:13 +0100

    Node changed to publish twist messages on cartesian_velocity_command topic

--- velocity_commands/src/velocity_commands_node.cpp
+++ velocity_commands/src/velocity_commands_node.cpp
@@ -1,27 +1,31 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <geometry_msgs/TwistStamped.h>
+#include <kdl/kdl.hpp>
 #include <sstream>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "velocity_commands_node");
-	ros::NodeHandle node_handle;
-	std::string cartesian_control_topic = "cartesian_velocity_command";
-	std_msgs::String msg;
-	ros::Publisher velocity_publisher = \
-	node_handle.advertise<std_msgs::String>("cartesian_control_topic", 1000);
+	ros::NodeHandle node_handle("~");
+	geometry_msgs::TwistStamped velocity;
 
-	ros::Rate loop_rate(10);
+	ros::Publisher velocity_publisher = \
+	node_handle.advertise<geometry_msgs::TwistStamped>("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command", 1);
+
+	ros::Rate loop_rate(50);
+	velocity.header.frame_id = "arm_link_5";
+	velocity.twist.linear.x = 0;
+	velocity.twist.linear.y = 10;
+	velocity.twist.linear.z = 0;
+	velocity.twist.angular.x = 0;
+	velocity.twist.angular.y = 0;
+	velocity.twist.angular.z = 0;
 
 	int count = 0;
 	while (ros::ok())
 	{
-		std::stringstream ss;
-		ss << "hello world " << count;
-		msg.data = ss.str();
-
-		ROS_INFO("%s", msg.data.c_str());
-		velocity_publisher.publish(msg);
+		velocity_publisher.publish(velocity);
 		ros::spinOnce();
 		loop_rate.sleep();
 		++count;
