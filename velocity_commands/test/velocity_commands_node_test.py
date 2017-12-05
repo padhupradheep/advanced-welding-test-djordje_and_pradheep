965e6be644091bd67ee4766a82a543f1a42eab23
padhupradheep <padhupradheep@gmail.com>
2017-12-05 18:38:21 +0100

    Unit tests are complete

--- velocity_commands/test/velocity_commands_node_test.py
+++ velocity_commands/test/velocity_commands_node_test.py
@@ -7,29 +7,36 @@ import rostest
 from std_msgs.msg import String
 from geometry_msgs.msg import TwistStamped
 PKG = 'velocity_commands'
-
 class TestVelocityCommand(unittest.TestCase):
 
     def setUp(self):
+        self.result = TwistStamped()
         self.wait_for_result = False
-        self.result = None
-        self.twist = TwistStamped()
         rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",TwistStamped,self.callback)
 
+
     def test1(self):
-        self.assertAlmostEqual(self.twist.twist.linear.x,0)
-        self.assertAlmostEqual(self.twist.twist.linear.y,0)
-        self.assertAlmostEqual(self.twist.twist.linear.z,0)
+        while  not self.wait_for_result:
+            pass
+        self.assertAlmostEqual(self.result.twist.linear.x,0)
+        self.assertAlmostEqual(self.result.twist.linear.y,0)
+        self.assertAlmostEqual(self.result.twist.linear.z,1)
 
     def test2(self):
-        self.assertAlmostEqual(self.twist.twist.angular.x,0)
-        self.assertAlmostEqual(self.twist.twist.angular.y,0)
-        self.assertAlmostEqual(self.twist.twist.angular.z,0)
+        while  not self.wait_for_result:
+            pass
+        self.assertAlmostEqual(self.result.twist.angular.x,0)
+        self.assertAlmostEqual(self.result.twist.angular.y,0)
+        self.assertAlmostEqual(self.result.twist.angular.z,0)
+
+    def test3(self):
+        while  not self.wait_for_result:
+            pass
+        self.assertEquals('arm_link_5', self.result.header.frame_id, "Incorrect link published")
 
     def callback(self,msg):
         self.result = msg
         self.wait_for_result = True
-
 
     def tearDown(self):
         pass
@@ -38,4 +45,3 @@ class TestVelocityCommand(unittest.TestCase):
 if __name__ == '__main__':
     rospy.init_node('velocity_commands_node_test')
     rostest.rosrun(PKG, 'velocity_commands_node_test', TestVelocityCommand)
-rospy.spin()
