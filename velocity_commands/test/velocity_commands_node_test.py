c4b3e56d8c02ce99904ca016fc638ac38fd9a2cd
padhupradheep <padhupradheep@gmail.com>
2017-12-05 01:33:30 +0100

    Unit tests are added

--- velocity_commands/test/velocity_commands_node_test.py
+++ velocity_commands/test/velocity_commands_node_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import ctypes
 import unittest
 import rosunit
 import rospy
@@ -14,12 +15,17 @@ class TestVelocityCommand(unittest.TestCase):
         self.wait_for_result = False
         self.result = None
         self.twist = TwistStamped()
-        rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",String,self.callback)
+        rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",TwistStamped,self.callback)
 
     def test1(self):
+        self.assertAlmostEqual(self.twist.twist.linear.x,0)
+        self.assertAlmostEqual(self.twist.twist.linear.y,0)
         self.assertAlmostEqual(self.twist.twist.linear.z,0)
 
-
+    def test2(self):
+        self.assertAlmostEqual(self.twist.twist.angular.x,0)
+        self.assertAlmostEqual(self.twist.twist.angular.y,0)
+        self.assertAlmostEqual(self.twist.twist.angular.z,0)
 
     def callback(self,msg):
         self.result = msg
