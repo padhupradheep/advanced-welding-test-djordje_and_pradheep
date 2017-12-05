7296fb2c3ed77d08652e91544968c0f6ab15baac
padhupradheep <padhupradheep@gmail.com>
2017-12-05 15:35:12 +0100

    Created a new branch-Test files are added

---
+++ velocity_commands/test/velocity_commands_node_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import unittest
+import rosunit
+import rospy
+import rostest
+from std_msgs.msg import String
+from geometry_msgs.msg import TwistStamped
+PKG = 'velocity_commands'
+
+class TestVelocityCommand(unittest.TestCase):
+
+    def setUp(self):
+        self.wait_for_result = False
+        self.result = None
+        self.twist = TwistStamped()
+        rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",String,self.callback)
+
+    def test1(self):
+        self.assertAlmostEqual(self.twist.twist.linear.z,0)
+
+
+
+    def callback(self,msg):
+        self.result = msg
+        self.wait_for_result = True
+
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    rospy.init_node('velocity_commands_node_test')
+    rostest.rosrun(PKG, 'velocity_commands_node_test', TestVelocityCommand)
+    rospy.spin()
