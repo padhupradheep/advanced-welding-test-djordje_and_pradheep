0ff3108e4016464b0405d0a2ff1a71197021d90e
padhupradheep <padhupradheep@gmail.com>
2017-12-05 15:41:40 +0100

    Changed the test file

--- velocity_commands/test/velocity_commands_node_test.py
+++ velocity_commands/test/velocity_commands_node_test.py
@@ -14,12 +14,17 @@ class TestVelocityCommand(unittest.TestCase):
         self.wait_for_result = False
         self.result = None
         self.twist = TwistStamped()
-        rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",String,self.callback)
+        rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",TwistStamped,self.callback)
 
     def test1(self):
+        self.assertAlmostEqual(self.twist.twist.linear.x,0)
+        self.assertAlmostEqual(self.twist.twist.linear.y,0)
         self.assertAlmostEqual(self.twist.twist.linear.z,0)
 
-
+    def test2(self):
+        self.assertAlmostEqual(self.twist.twist.angular.x,0)
+        self.assertAlmostEqual(self.twist.twist.angular.y,0)
+        self.assertAlmostEqual(self.twist.twist.angular.z,0)
 
     def callback(self,msg):
         self.result = msg
@@ -33,4 +38,4 @@ class TestVelocityCommand(unittest.TestCase):
 if __name__ == '__main__':
     rospy.init_node('velocity_commands_node_test')
     rostest.rosrun(PKG, 'velocity_commands_node_test', TestVelocityCommand)
-    rospy.spin()
+rospy.spin()
