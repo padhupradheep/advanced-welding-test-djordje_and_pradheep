670b966de681c8c29b1910c56725e5d19b475f27
Djordje Vukcevic <djordje.vukcevic@smail.inf.h-brs.de>
2017-12-06 11:01:25 +0100

    Changing catkin build to catkin_make in travis setup

--- velocity_commands/src/velocity_commands_node.cpp
+++ velocity_commands/src/velocity_commands_node.cpp
@@ -29,41 +29,41 @@ SOFTWARE.
 
 int main(int argc, char **argv)
 {
-   ros::init(argc, argv, "velocity_commands_node");
- double linear_x = 0;
- double linear_y = 0;
- double linear_z = 0;
- double angular_x = 0;
- double angular_y = 0;
- double angular_z = 0;
+    ros::init(argc, argv, "velocity_commands_node");
+    double linear_x = 0;
+    double linear_y = 0;
+    double linear_z = 0;
+    double angular_x = 0;
+    double angular_y = 0;
+    double angular_z = 0;
 
- ros::NodeHandle node_handle("~");
- node_handle.getParam("linear_x", linear_x);
- node_handle.getParam("linear_y", linear_y);
- node_handle.getParam("linear_z", linear_z);
- node_handle.getParam("angular_x", angular_x);
- node_handle.getParam("angular_y", angular_y);
- node_handle.getParam("angular_z", angular_z);
+    ros::NodeHandle node_handle("~");
+    node_handle.getParam("linear_x", linear_x);
+    node_handle.getParam("linear_y", linear_y);
+    node_handle.getParam("linear_z", linear_z);
+    node_handle.getParam("angular_x", angular_x);
+    node_handle.getParam("angular_y", angular_y);
+    node_handle.getParam("angular_z", angular_z);
 
- geometry_msgs::TwistStamped velocity;
+    geometry_msgs::TwistStamped velocity;
 
- ros::Publisher velocity_publisher = \
- node_handle.advertise<geometry_msgs::TwistStamped>("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command", 1);
+    ros::Publisher velocity_publisher = \
+    node_handle.advertise<geometry_msgs::TwistStamped>("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command", 1);
 
- ros::Rate loop_rate(50);
- velocity.header.frame_id = "arm_link_5";
- velocity.twist.linear.x = linear_x;
- velocity.twist.linear.y = linear_y;
- velocity.twist.linear.z = linear_z;
- velocity.twist.angular.x = angular_x;
- velocity.twist.angular.y = angular_y;
- velocity.twist.angular.z = angular_z;
+    ros::Rate loop_rate(50);
+    velocity.header.frame_id = "gripper_motor_mount_link";
+    velocity.twist.linear.x = linear_x;
+    velocity.twist.linear.y = linear_y;
+    velocity.twist.linear.z = linear_z;
+    velocity.twist.angular.x = angular_x;
+    velocity.twist.angular.y = angular_y;
+    velocity.twist.angular.z = angular_z;
 
- while (ros::ok())
- {
-   velocity_publisher.publish(velocity);
-   ros::spinOnce();
-   loop_rate.sleep();
- }
+    while (ros::ok())
+    {
+    velocity_publisher.publish(velocity);
+    ros::spinOnce();
+    loop_rate.sleep();
+    }
  return 0;
 }
--- velocity_commands/test/velocity_commands_node_test.py
+++ velocity_commands/test/velocity_commands_node_test.py
@@ -14,23 +14,22 @@ class TestVelocityCommand(unittest.TestCase):
         self.wait_for_result = False
         rospy.Subscriber("/mcr_manipulation/mcr_arm_cartesian_control/cartesian_velocity_command",TwistStamped,self.callback)
 
-
     def test1(self):
-        while  not self.wait_for_result:
+        while not self.wait_for_result:
             pass
         self.assertAlmostEqual(self.result.twist.linear.x,0)
         self.assertAlmostEqual(self.result.twist.linear.y,0)
         self.assertAlmostEqual(self.result.twist.linear.z,1)
 
     def test2(self):
-        while  not self.wait_for_result:
+        while not self.wait_for_result:
             pass
         self.assertAlmostEqual(self.result.twist.angular.x,0)
         self.assertAlmostEqual(self.result.twist.angular.y,0)
         self.assertAlmostEqual(self.result.twist.angular.z,0)
 
     def test3(self):
-        while  not self.wait_for_result:
+        while not self.wait_for_result:
             pass
         self.assertEquals('arm_link_5', self.result.header.frame_id, "Incorrect link published")
 
