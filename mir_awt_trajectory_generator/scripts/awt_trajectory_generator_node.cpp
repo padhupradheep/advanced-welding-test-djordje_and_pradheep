a61af86e3eac2d3b9064342da81b3b0a70b51434
Djordje Vukcevic <vukcevicdjordjemne@gmail.com>
2018-01-28 20:18:33 +0100

    Sine trajectory execution on real robot completed succefully.  (#10)
    * Sine trajectory execution on real robot completed succefully. Additional parameters added: velocity gains, side pointers and number of cycles.
    
    Square trajectory left for testing on real robot
    
    * Changed CMakelist for visualisation markers
    
    * Changed package.xml for visualisation markers
    
    * Interactive markers are up and running
    
    * Launch files changed
    
    * Interactive markers are in the same node
    
    * Launch files are changed and added
    
    * Packages are added
    
    * Interactive markers are visualized
    
    * Arm is moving with customized velocities
    
    * Node for simulating arm velocities created...time duration makes problem
    
    * Final commit

--- mir_awt_trajectory_generator/scripts/awt_trajectory_generator_node.cpp
+++ mir_awt_trajectory_generator/scripts/awt_trajectory_generator_node.cpp
@@ -2,17 +2,14 @@
 Created on: December, 2017
 Author(s): Djordje Vukcevic, Pradheep Padmanabhan
 Copyright (c) [2017]
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,15 +27,16 @@ SOFTWARE.
 #include <sstream>
 #include <Eigen/Dense>
 #include <awt_trajectory_generator.h>
+#include "visualization_msgs/Marker.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "awt_trajectory_generator_node");
-    std::string type;
+    std::string type, side;
     double phase = 0;
     double amplitude = 0;
-    int num_steps = 0;
-    double time_step = 0;
+    int num_steps = 0, side_gain = 1, cycles = 1;
+    double time_step = 0, loop_freq = 0, x_unit_gain = 0, y_unit_gain = 0;
     Eigen::MatrixXd velocity_matrix;
     std::string root_frame = "arm_link_0";
 
@@ -48,32 +46,103 @@ int main(int argc, char **argv)
     node_handle.getParam("amplitude", amplitude);
     node_handle.getParam("num_steps", num_steps);
     node_handle.getParam("time_step", time_step);
+    node_handle.getParam("loop_freq", loop_freq);
+    node_handle.getParam("x_unit_gain", x_unit_gain);
+    node_handle.getParam("y_unit_gain", y_unit_gain);
+    node_handle.getParam("side", side);
+    node_handle.getParam("cycles", cycles);
 
     geometry_msgs::TwistStamped velocity;
 
     ros::Publisher velocity_publisher = \
     node_handle.advertise<geometry_msgs::TwistStamped>("/arm_1/arm_controller/cartesian_velocity_command", 100);
 
+    ros::Publisher marker_pub = \
+    node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 100);
     traj_gen::AWT_Trajectory_Generator generator;
+
+    double step_size_x;
+    Eigen::MatrixXd wave;
+
+    if (type == "sine"){
+        generator.generate_sine_wave(
+                    phase,
+                    amplitude,
+                    num_steps,
+                    wave,
+                    step_size_x);
+    }
+
+    else if (type == "square" ){
+        generator.generate_square_wave(
+                    phase,
+                    amplitude,
+                    num_steps,
+                    wave,
+                    step_size_x);
+    }
+
+
     generator.generate_trajectory(type,
                                 phase,
                                 amplitude,
                                 num_steps,
                                 time_step,
                                 velocity_matrix);
+    if (side == "right") side_gain = -1;
+    else side_gain = 1;
 
-    ROS_INFO_STREAM("The publisher need some time to connect to subscribers.");
+    ROS_INFO_STREAM("The marker publisher need some time to connect to subscribers.");
+    while(!marker_pub.getNumSubscribers() > 0){}
+    ROS_INFO_STREAM("The marker publisher has connected to subscribers.");
+
+    ROS_INFO_STREAM("The velocity publisher need some time to connect to subscribers.");
     while(!velocity_publisher.getNumSubscribers() > 0){}
-    ROS_INFO_STREAM("The publisher has connected to subscribers.");
+    ROS_INFO_STREAM("The velocity publisher has connected to subscribers.");
 
 
-    ros::Rate loop_rate(1/time_step);
+    ros::Rate loop_rate(loop_freq);
+    ros::Rate loop_rate_markers(20);
+
     velocity.header.frame_id = root_frame;
+    int step_counter = 0;
+    visualization_msgs::Marker marker;
+    marker.id = 0;
 
-    for (int i = 0;  i < velocity_matrix.rows(); i++)
+    for (int i = 0; i < velocity_matrix.rows()*cycles; i++)
     {
-        velocity.twist.linear.x = velocity_matrix(i, 0);
-        velocity.twist.linear.y = velocity_matrix(i, 1);
+        if (step_counter == velocity_matrix.rows()) step_counter = 0;
+        marker.header.frame_id = "arm_link_5";
+        marker.type = visualization_msgs::Marker::SPHERE;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = (wave(step_counter, 1)/x_unit_gain)*0.05;
+        marker.pose.position.y = (side_gain * wave(step_counter, 0)/y_unit_gain)*0.1;
+        marker.pose.position.z = 0.0;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 0.0;
+        marker.scale.x = 0.02;
+        marker.scale.y = 0.01;
+        marker.scale.z = 0.008;
+        marker.color.r = 0.0;
+        marker.color.g = 2.0;
+        marker.color.b = 0.0;
+        marker.color.a = 1.0;
+        marker.lifetime = ros::Duration();
+        marker.id+=1;
+        step_counter++;
+        marker_pub.publish(marker);
+        loop_rate_markers.sleep();
+    }
+
+    step_counter = 0;
+    for (int i = 0;  i < velocity_matrix.rows() * cycles; i++)
+    {
+        if (step_counter == velocity_matrix.rows()) step_counter = 0;
+        velocity.twist.linear.x = velocity_matrix(step_counter, 1)/x_unit_gain;
+        velocity.twist.linear.y = side_gain * velocity_matrix(step_counter, 0)/y_unit_gain;
+        step_counter++;
         velocity_publisher.publish(velocity);
         ros::spinOnce();
         loop_rate.sleep();
